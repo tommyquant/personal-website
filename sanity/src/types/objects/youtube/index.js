3d0cd7d35ce113e078fcff93c4fa7ddbe1428bbd
Tommy <51040159+tommyquant@users.noreply.github.com>
2020-03-23 16:50:53 +1100

    Source of truth for youtube embed object name

--- sanity/src/types/objects/youtube/index.js
+++ sanity/src/types/objects/youtube/index.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import getYouTubeId from 'get-youtube-id';
-import YouTube from 'react-youtube';
 
-const Preview = ({value}) => {
-    const { url } = value;
-    const id = getYouTubeId(url);
-    return (<YouTube videoId={id} />);
-};
+import Preview from './component';
 
-Preview.propTypes = {
-    value: PropTypes.shape({
-        url: PropTypes.string.isRequired
-    })
-};
+export const reference = 'youtube';
 
 export default {
-    name: 'youtube',
-    type: 'object',
     title: 'YouTube Embed',
+    name,
+    type: 'object',
     fields: [
         {
             name: 'url',
