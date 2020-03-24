e56ef05cd32487bb22f4342d461694c4e15257b9
Tommy <tommyquant@gmail.com>
2020-03-24 15:35:08 +1100

    Set up Apollo and basic home page

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-const Home = ({
-    data
-}) => {
+const Image = styled.img`
+    display: block;
+    width: 200px;
+`;
+
+const query = gql`
+    {
+        allPost {
+            title
+            feature_image {
+                asset {
+                    metadata {
+                        lqip
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const Home = () => {
+    const { loading, error, data } = useQuery(query);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
     return (
-        <span>Hello World!</span>
+        <div>
+            Hello! {!!data.allPost && data.allPost.map(({ title, feature_image }) => (
+                <Image key={title} alt={title} src={feature_image.asset.metadata.lqip} />
+            ))}
+        </div>
     );
-};
-
-Home.propTypes = {
-    data: PropTypes.any
 };
 
 export default Home;
---
+++ app/src/root-element-wrapper.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+import fetch from 'isomorphic-fetch';
+
+const client = new ApolloClient({
+    uri: `https://${process.env.SANITY_PROJECT_ID}.apicdn.sanity.io/v1/graphql/${process.env.SANITY_DATASET}/default`,
+    fetch // Use the same fetch API for SSR and browser
+});
+
+// eslint-disable-next-line react/prop-types
+const RootElementWrapper = ({ element }) => (
+    <ApolloProvider client={client}>
+        {element}
+    </ApolloProvider>
+);
+
+export default RootElementWrapper;
