ad1ada4a9f2a4bacdd285854070db6eeb905a6dd
Tommy <tommyquant@gmail.com>
2020-03-26 17:10:10 +1100

    Move common style utils to common/style folder

--- app/src/widgets/root/root-element-wrapper.js
+++ app/src/widgets/root/root-element-wrapper.js
@@ -7,7 +7,7 @@ import {createGlobalStyle} from 'styled-components';
 import styledNormalize from 'styled-normalize';
 import 'typeface-roboto';
 
-import {accent, copy, pageBg} from '../../common/palette';
+import {accent, copy, pageBg} from '../../common/style/palette';
 
 const client = new ApolloClient({
     uri: `https://${process.env.SANITY_PROJECT_ID}.apicdn.sanity.io/v1/graphql/${process.env.SANITY_DATASET}/default`,
