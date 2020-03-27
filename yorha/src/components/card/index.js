11c3661c37b0ab69d99f2e87427fb55dcec3adb4
Tommy <tommyquant@gmail.com>
2020-03-27 18:34:08 +1100

    Refactor Button and add additional states

--- yorha/src/components/card/index.js
+++ yorha/src/components/card/index.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import styled, {css} from 'styled-components';
 import {rgba} from 'polished';
 
+import {fontFamily} from '../../common/style/font';
 import {tana, fuscousGray} from '../../common/style/palette';
 
 const Wrapper = styled.div`
     background-color: ${tana};
+    font-family: ${fontFamily};
 
     ${({shadow}) => shadow && css`
         box-shadow: 3px 3px 0 0 ${rgba(fuscousGray, 0.5)};
