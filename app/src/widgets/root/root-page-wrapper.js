cf90444b3a606800f1588cca23bc4a11a5e6c712
Tommy <tommyquant@gmail.com>
2020-03-31 18:25:25 +1100

    #10 | Create PageDivider

--- app/src/widgets/root/root-page-wrapper.js
+++ app/src/widgets/root/root-page-wrapper.js
@@ -2,46 +2,60 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import {fuscousGray, taupeGray} from 'yorha/src/common/style/palette';
+import {softAmber, taupeGray} from 'yorha/src/common/style/palette';
 import DoubleBarLine from 'yorha/src/components/graphics/double-bar-line';
 import Button from 'yorha/src/components/button';
+import PageDivider from 'yorha/src/components/graphics/page-divider';
 
 import {
     PAGE_HORIZONTAL_EDGE_SPACING,
     PAGE_VERTICAL_EDGE_SPACING
 } from '../../common/style/constants';
 
-const NavTop = styled.div`
-    border-bottom: 2px solid ${fuscousGray};
+const Navigation = styled.nav`
+    position: sticky;
+    top: 0;
+    z-index: 1;
 `;
 
 const NavButtons = styled.div`
+    background-color: ${softAmber};
     display: grid;
     grid-auto-columns: max-content;
     grid-auto-flow: column;
-    grid-gap: 2em;
-    padding: ${PAGE_VERTICAL_EDGE_SPACING} ${PAGE_HORIZONTAL_EDGE_SPACING} 1.2em;
+    grid-gap: 2rem;
+    padding: ${PAGE_VERTICAL_EDGE_SPACING} ${PAGE_HORIZONTAL_EDGE_SPACING} 1.2rem;
+`;
+
+const StyledButton = styled(Button)`
+    text-transform: uppercase;
+`;
+
+const StyledPageDivider = styled(PageDivider)`
+    box-sizing: border-box;
+    margin-bottom: 1.5rem;
+    padding: 0 ${PAGE_HORIZONTAL_EDGE_SPACING};
 `;
 
 const Content = styled.section`
-    padding: 0 ${PAGE_HORIZONTAL_EDGE_SPACING} 2em;
+    padding: 0 ${PAGE_HORIZONTAL_EDGE_SPACING} 2rem;
 `;
 
 const RootPageWrapper = ({element}) => (
     <main>
-        <nav>
-            <NavTop>
-                <NavButtons>
-                    <DoubleBarLine color={taupeGray} />
-                    <Button isActive={true}>Home</Button>
-                    <Button>Projects</Button>
-                    <Button>About</Button>
-                </NavButtons>
-            </NavTop>
-        </nav>
+        <Navigation>
+            <NavButtons>
+                <DoubleBarLine color={taupeGray} />
+                <StyledButton isActive={true}>Home</StyledButton>
+                <StyledButton>Projects</StyledButton>
+                <StyledButton>About</StyledButton>
+            </NavButtons>
+            <StyledPageDivider />
+        </Navigation>
         <Content>
             {element}
         </Content>
+        <StyledPageDivider />
     </main>
 );
 
