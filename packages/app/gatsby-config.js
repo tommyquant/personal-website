f8284c306fd9b685903057a0b91704216e6b8930
tommyquant <51040159+tommyquant@users.noreply.github.com>
2020-04-05 18:46:20 +1000

    #34 | Convert to Yarn (#35)
    * Move all packages to packages folder
    * Set up Yarn workspace

---
+++ packages/app/gatsby-config.js
@@ -0,0 +1,24 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+    siteMetadata: {
+        title: 'Tommy Tran'
+    },
+    plugins: [
+        {
+            resolve: 'gatsby-plugin-styled-components'
+        },
+        {
+            resolve: 'gatsby-plugin-create-client-paths',
+            options: {prefixes: ['/posts/*']},
+        }
+    ]
+};
