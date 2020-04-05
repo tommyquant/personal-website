eceaa3bdf667e012fd913144d633a873e2ae55ed
tommyquant <51040159+tommyquant@users.noreply.github.com>
2020-04-05 23:28:50 +1000

    #36 | Set up Docker containers (#37)

--- packages/app/src/common/image-url-builder.js
+++ packages/app/src/common/image-url-builder.js
@@ -1,8 +1,8 @@
 import imageUrlBuilder from '@sanity/image-url';
 
 const builder = imageUrlBuilder({
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID,
-    dataset: process.env.GATSBY_SANITY_DATASET
+    projectId: process.env.SANITY_STUDIO_API_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_API_DATASET
 });
 
 export default builder;
