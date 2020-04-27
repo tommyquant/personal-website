0ac04480dc60bbf6568399e130588a86e8312bc7
Tommy <51040159+tommyquant@users.noreply.github.com>
2020-04-27 05:49:35 +0000

    #53 | Fix redirects for /studio

--- tasks/package-configs/build.js
+++ tasks/package-configs/build.js
@@ -13,8 +13,7 @@ module.exports = {
             require(`${path.resolve(baseConfig.app.directory)}/client-routes`)
                 .map((clientRoute) => {
                     return `${clientRoute} ${clientRoute.replace('*', '')} 200`
-                }),
-            '/* /404/ 404'
+                })
         ]
     },
     sanity: {
@@ -26,6 +25,7 @@ module.exports = {
             args: `public${process.env.STUDIO_BASE_PATH} --yes`
         },
         redirects: [
+            `${process.env.STUDIO_BASE_PATH}/static/* ${process.env.STUDIO_BASE_PATH}/static/:splat 200`,
             `${process.env.STUDIO_BASE_PATH}/* ${process.env.STUDIO_BASE_PATH}/index.html 200`
         ]
     },
