f4d65cb8cde7a8b1940a6d2a1e82b7a7ff5694bb
tommyquant <51040159+tommyquant@users.noreply.github.com>
2020-04-27 19:24:28 +1000

    #24 | Loading states (#55)

--- packages/yorha/src/components/graphics/page-divider/index.stories.js
+++ packages/yorha/src/components/graphics/page-divider/index.stories.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
 import {color} from '@storybook/addon-knobs';
 
 import {fuscousGray} from '../../../common/style/palette';
 import PageDivider from '.';
-
-const Wrapper = styled.div`
-    padding: 0 2rem;
-    width: 100%;
-`;
 
 export default {
     title: 'Components/Graphics'
 };
 
 export const pageDivider = () => (
-    <Wrapper>
-        <PageDivider color={color('Color', fuscousGray)} />
-    </Wrapper>
+    <PageDivider color={color('Color', fuscousGray)} />
 );
